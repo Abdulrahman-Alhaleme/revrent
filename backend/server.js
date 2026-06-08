5169c39d97cac07f2195f4810078d7ec3017a1a7
Abdulrahman-Alhaleme <abdulrahman.alhaleme@gmail.com>
2026-06-08 10:41:13 +0300

    fix: use PORT 8080 as default

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
